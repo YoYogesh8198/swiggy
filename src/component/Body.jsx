221db96345cc22bb177d3674b9cfd9d28e43f171
Yog esh <jangiry035@gmail.com>
2024-07-16 22:21:35 +0530

    first commit

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Body = () => {
-  return (
-    <div>Body</div>
-  )
+     const [data, setData] = useState([]);
+     async function fetchData() {
+          const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+          const result = await data.json();
+          console.log(result?.data);
+     }
+     useEffect(()=>{
+          fetchData()
+     },[])
+     return (
+          <div className='w-full '>
+               <div className='w-[80%] mx-auto'>div</div>
+          </div>
+     )
 }
 
 export default Body
--- src/component/Head.jsx
+++ src/component/Head.jsx
@@ -8,34 +8,34 @@ const Head = () => {
                          <img className='w-20 hover:scale-110 ease-in-out duration-500' src="public/Swiggylogo.png" alt="" />
                          <div className='flex items-center justify-center gap-3'>
                               <p className='cursor-pointer border-b-2 border-black hover:text-orange-500 hover:border-orange-500 font-bold'>Other</p>
-                              <i class="fi fi-rs-angle-small-down mt-1  text-orange-500 text-2xl"></i>
+                              <i className="fi fi-rs-angle-small-down mt-1  text-orange-500 text-2xl"></i>
                          </div>
 
                     </div>
 
                     <div className='flex justify-between gap-10'>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-br-briefcase"></i>
+                              <i className="fi mt-1 fi-br-briefcase"></i>
                               <p>Swiggy Corporate</p>
                          </div>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-br-search"></i>
+                              <i className="fi mt-1 fi-br-search"></i>
                               <p>Search</p>
                          </div>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-br-badge-percent"></i>
+                              <i className="fi mt-1 fi-br-badge-percent"></i>
                               <p>Offers</p>
                          </div>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-br-info"></i>
+                              <i className="fi mt-1 fi-br-info"></i>
                               <p>Help</p>
                          </div>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-rs-user"></i>
+                              <i className="fi mt-1 fi-rs-user"></i>
                               <p>Sign In</p>
                          </div>
                          <div className='cursor-pointer flex items-center justify-center gap-2 hover:text-orange-500 font-semibold'>
-                              <i class="fi mt-1 fi-br-cart-shopping-fast"></i>
+                              <i className="fi mt-1 fi-br-cart-shopping-fast"></i>
                               <p>Cart</p>
                          </div>
                     </div>
